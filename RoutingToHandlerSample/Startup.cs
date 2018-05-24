c1d5efd515a5c375a94f11eb1a89b430dfe37eb9
Kiran Challa <kichalla@microsoft.com>
2018-05-24 15:25:06 -0700

    Initial

--- RoutingToHandlerSample/Startup.cs
+++ RoutingToHandlerSample/Startup.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RoutingToHandlerSample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseRouter(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("", (httpContext) =>
+                {
+                    var response = httpContext.Response;
+                    response.StatusCode = StatusCodes.Status200OK;
+                    return response.WriteAsync("Hello, World!", httpContext.RequestAborted);
+                });
             });
         }
     }
