11d2cdbf7c92fe59c50f31b857f351c8e68357b3
Kiran Challa <kichalla@microsoft.com>
2018-05-30 15:19:04 -0700

    Return a status code without returning a result

--- RoutingToHandlerSample/Controllers/ConventionalControllerDerivingFromControllerBase.cs
+++ RoutingToHandlerSample/Controllers/ConventionalControllerDerivingFromControllerBase.cs
@@ -13,11 +13,21 @@ namespace RoutingToHandlerSample.Controllers
         //    //throw new InvalidOperationException("Error in Customers.Index");
         //}
 
-        public Task Index()
-        {
-            HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+        //public Task Index()
+        //{
+        //    HttpContext.Response.StatusCode = StatusCodes.Status200OK;
 
-            return HttpContext.Response.WriteAsync("Hello, World!");
+        //    return HttpContext.Response.WriteAsync("Hello, World!");
+        //}
+
+        public IActionResult Index()
+        {
+            return StatusCode(StatusCodes.Status200OK);
         }
+
+        //public Task Index()
+        //{
+        //    return Task.CompletedTask;
+        //}
     }
 }
