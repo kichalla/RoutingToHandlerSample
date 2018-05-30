3a22c3a42581e2853f4ceab6784c73cc15fd3d0b
Kiran Challa <kichalla@microsoft.com>
2018-05-30 15:11:07 -0700

    Writing to response directly from controller's action

--- RoutingToHandlerSample/Controllers/AttributeRouteControllerDerivingFromControllerBase.cs
+++ RoutingToHandlerSample/Controllers/AttributeRouteControllerDerivingFromControllerBase.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using RoutingToHandlerSample.Internal;
 using System;
+using System.Threading.Tasks;
 
 namespace RoutingToHandlerSample.Controllers
 {
     public class AttributeRouteControllerDerivingFromControllerBase : ControllerBase
     {
-        //[CustomAuthFilter]
+        ////[CustomAuthFilter]
+        //[Route("/AttributeRouteControllerDerivingFromControllerBase/index")]
+        //public IActionResult Index()
+        //{
+        //    return Content("Hello, World!");
+        //    //throw new InvalidOperationException("Error in Customers.Index");
+        //}
+
         [Route("/AttributeRouteControllerDerivingFromControllerBase/index")]
-        public IActionResult Index()
+        public Task Index()
         {
-            return Content("Hello, World!");
-            //throw new InvalidOperationException("Error in Customers.Index");
+            HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+
+            return HttpContext.Response.WriteAsync("Hello, World!");
         }
     }
 }
--- RoutingToHandlerSample/Controllers/ConventionalControllerDerivingFromControllerBase.cs
+++ RoutingToHandlerSample/Controllers/ConventionalControllerDerivingFromControllerBase.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace RoutingToHandlerSample.Controllers
 {
     public class ConventionalControllerDerivingFromControllerBase : ControllerBase
     {
-        //[CustomAuthFilter]
-        public IActionResult Index()
+        ////[CustomAuthFilter]
+        //public IActionResult Index()
+        //{
+        //    return Content("Hello, World!");
+        //    //throw new InvalidOperationException("Error in Customers.Index");
+        //}
+
+        public Task Index()
         {
-            return Content("Hello, World!");
-            //throw new InvalidOperationException("Error in Customers.Index");
+            HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+
+            return HttpContext.Response.WriteAsync("Hello, World!");
         }
     }
 }
