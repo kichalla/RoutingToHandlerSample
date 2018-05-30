96e28b0730644f25c2b5ceee0f7995acaa1a1d95
Kiran Challa <kichalla@microsoft.com>
2018-05-30 12:53:09 -0700

    Added controllers which derive from controller base

---
+++ RoutingToHandlerSample/Controllers/AttributeRouteControllerDerivingFromControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoutingToHandlerSample.Internal;
+using System;
+
+namespace RoutingToHandlerSample.Controllers
+{
+    public class AttributeRouteControllerDerivingFromControllerBase : ControllerBase
+    {
+        //[CustomAuthFilter]
+        [Route("/AttributeRouteControllerDerivingFromControllerBase/index")]
+        public IActionResult Index()
+        {
+            return Content("Hello, World!");
+            //throw new InvalidOperationException("Error in Customers.Index");
+        }
+    }
+}
---
+++ RoutingToHandlerSample/Controllers/ConventionalControllerDerivingFromControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingToHandlerSample.Controllers
+{
+    public class ConventionalControllerDerivingFromControllerBase : ControllerBase
+    {
+        //[CustomAuthFilter]
+        public IActionResult Index()
+        {
+            return Content("Hello, World!");
+            //throw new InvalidOperationException("Error in Customers.Index");
+        }
+    }
+}
